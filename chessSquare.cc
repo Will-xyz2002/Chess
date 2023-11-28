77ec4cf95e50e9533e3320677f9ae287612fe416
gichihas <gichihas@uwaterloo.ca>
2023-11-28 13:25:16 -0500

    chessSqure.cc modified

---
+++ chessSquare.cc
@@ -0,0 +1,11 @@
+#include "chessSquare.h"
+
+ChessSquare::ChessSquare (int r, int c, bool empty)
+    : row{r}, column{c} {}
+
+int ChessSquare::getRow() { return row; }
+int ChessSquare::getColumn() { return column; }
+void ChessSquare::setCoords(const int row, const int column) {
+    row = row;
+    column = column;
+}
