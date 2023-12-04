479fa9a289c68319be4b239e61458dfbb4ff0597
Harry Lam <htlam@uwaterloo.ca>
2023-12-03 19:42:10 -0500

    remove board.init inside chessGame - this have been done in main

--- chessGame.cc
+++ chessGame.cc
@@ -15,9 +15,6 @@ bool checkValidRow(std::string position) {
 ChessGame::ChessGame(ChessBoard board, bool whiteTurn, Player p1, Player p2): 
                     board{board}, whiteTurn{whiteTurn}, p1{p1}, p2{p2} {
 
-    // Initialize the board with pieces
-    this->board.init();
-
     // initialize text display, set it and output it
     this->textDisplay = make_unique<TextDisplay>();
     this->textDisplay->setBoard(board);
@@ -30,7 +27,7 @@ ChessGame::ChessGame(ChessBoard board, bool whiteTurn, Player p1, Player p2):
     this->graphicsDisplay = make_unique<GraphicsDisplay>(new Xwindow(560, 560), BOARD_DIMENSION);
     this->graphicsDisplay->setBoard(board);
     this->board.attach(graphicsDisplay.get()); 
-    this->graphicsDisplay->drawGrid();
+    this->graphicsDisplay->drawGrid(); 
 }
 
 ChessGame::~ChessGame() {}
