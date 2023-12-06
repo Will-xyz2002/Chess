86db3cf2f381b440b99aa5f6ad86bd165c43d5d4
gichihas <gichihas@uwaterloo.ca>
2023-12-05 21:01:57 -0500

    point changed

--- chessBoard.cc
+++ chessBoard.cc
@@ -601,12 +601,12 @@ vector<ChessMove> ChessBoard::avoidAttackGenerator(ChessPiece p){
 }
 
 int ChessBoard::piecePoint(ChessType type) {
-    if(type == ChessType::King) { return 50; }
-    if(type == ChessType::Queen) { return 10; }
-    if(type == ChessType::Bishop) { return 5; }
-    if(type == ChessType::Rook) { return 5; }
-    if(type == ChessType::Knight) { return 3; }
     if(type == ChessType::Pawn) { return 1; }
+    if(type == ChessType::Knight) { return 3; }
+    if(type == ChessType::Bishop) { return 3; }
+    if(type == ChessType::Rook) { return 5; }
+    if(type == ChessType::Queen) { return 9; }
+    if(type == ChessType::King) { return 50; }
     return 0;
 }
 
